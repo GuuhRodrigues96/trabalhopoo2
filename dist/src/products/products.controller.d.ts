8122885daefe2faa8bb714c7686cebb7b0cfc974
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:01:27 -0300

    Update products.controller.d.ts

--- dist/src/products/products.controller.d.ts
+++ dist/src/products/products.controller.d.ts
@@ -1,3 +1,18 @@
+/**
+ * Controlador responsável pelo gerenciamento dos produtos no sistema.
+ * 
+ * O `ProductsController` lida com as requisições HTTP relacionadas aos produtos, 
+ * incluindo a criação, consulta, atualização e remoção de produtos. Ele delega 
+ * essas operações para o `ProductsService`.
+ * 
+ * Métodos:
+ * - `create(createProductDto: { ean: string, name: string, idCategory: number })`: Cria um novo produto.
+ * - `findAll()`: Retorna todos os produtos cadastrados.
+ * - `findOne(id: string)`: Retorna um produto específico com base no ID.
+ * - `update(id: string, updateProductDto: { ean?: string, name?: string, idCategory?: number })`: Atualiza os dados de um produto existente.
+ * - `remove(id: string)`: Remove um produto com base no ID.
+ */
+
 import { ProductsService } from './products.service';
 import { Product } from '@prisma/client';
 export declare class ProductsController {
