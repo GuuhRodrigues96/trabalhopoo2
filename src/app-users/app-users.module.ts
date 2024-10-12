d8e523de82194e2df36a5bc3e459e3b171ecaf02
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:16:33 -0300

    Update app-users.module.ts

--- src/app-users/app-users.module.ts
+++ src/app-users/app-users.module.ts
@@ -1,3 +1,13 @@
+/**
+ * Módulo que encapsula a lógica relacionada aos usuários do aplicativo.
+ * 
+ * O `AppUsersModule` é responsável por agrupar o controlador e o serviço
+ * que gerenciam as operações de usuários. Ele registra o `AppUsersController`
+ * para lidar com as requisições HTTP e o `AppUsersService` para implementar 
+ * a lógica de negócios relacionada a esses usuários. O `PrismaService` é
+ * também fornecido como uma dependência, permitindo acesso ao banco de dados.
+ */
+
 import { Module } from '@nestjs/common';
 import { AppUsersController } from './app-users.controller';
 import { AppUsersService } from './app-users.service';
