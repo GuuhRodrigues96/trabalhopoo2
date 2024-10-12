7b2f1123b427f2af01da13aac26fc8f302646e8d
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:15:55 -0300

    Update app-users.controller.ts

--- src/app-users/app-users.controller.ts
+++ src/app-users/app-users.controller.ts
@@ -1,3 +1,18 @@
+/**
+ * Controlador responsável pelas operações relacionadas aos usuários do aplicativo.
+ * 
+ * O `AppUsersController` fornece endpoints para criar, buscar, atualizar e remover
+ * usuários. Ele utiliza o `AppUsersService` para realizar a lógica de negócios
+ * associada a essas operações.
+ * 
+ * Rotas:
+ * - `POST /app-users`: Cria um novo usuário.
+ * - `GET /app-users`: Retorna todos os usuários.
+ * - `GET /app-users/:id`: Retorna um usuário específico pelo ID.
+ * - `PUT /app-users/:id`: Atualiza um usuário específico pelo ID.
+ * - `DELETE /app-users/:id`: Remove um usuário específico pelo ID.
+ */
+
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { AppUsersService } from './app-users.service';
 import { AppUser } from '@prisma/client';
