acf0bacf1075aef8771b8a296af28e59a93524ae
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:08:03 -0300

    Update app.module.d.ts

--- dist/src/app.module.d.ts
+++ dist/src/app.module.d.ts
@@ -1,2 +1,11 @@
+/**
+ * Módulo principal da aplicação.
+ * 
+ * O `AppModule` encapsula todas as partes essenciais do aplicativo, 
+ * incluindo controladores, serviços e outros módulos necessários. 
+ * Ele organiza a estrutura da aplicação e permite que o NestJS 
+ * faça a injeção de dependências e o gerenciamento de módulos.
+ */
+
 export declare class AppModule {
 }
