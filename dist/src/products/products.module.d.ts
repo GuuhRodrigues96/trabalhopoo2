90af40fb5a90733518374bc773196954220cb4c7
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:03:42 -0300

    Update products.module.d.ts

--- dist/src/products/products.module.d.ts
+++ dist/src/products/products.module.d.ts
@@ -1,2 +1,10 @@
+/**
+ * Módulo responsável por gerenciar a funcionalidade de produtos no sistema.
+ * 
+ * O `ProductsModule` encapsula todas as operações e serviços relacionados
+ * aos produtos do aplicativo. Ele organiza e disponibiliza o controlador e 
+ * o serviço de produtos para serem utilizados em outras partes da aplicação.
+ */
+
 export declare class ProductsModule {
 }
