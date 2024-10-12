284ceb7628aa039b359ef6a670b092b4c8169302
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 21:51:43 -0300

    Update prisma.service.d.ts

--- dist/prisma/prisma.service.d.ts
+++ dist/prisma/prisma.service.d.ts
@@ -1,3 +1,11 @@
+/**
+ * Serviço Prisma que gerencia a conexão e as operações com o banco de dados.
+ * 
+ * Esta classe estende o PrismaClient, fornecendo funcionalidades de ORM para 
+ * interagir com o banco de dados. É utilizada em toda a aplicação para realizar
+ * consultas e manipulações de dados.
+ */
+
 import { PrismaClient } from '@prisma/client';
 export declare class PrismaService extends PrismaClient {
     constructor();
