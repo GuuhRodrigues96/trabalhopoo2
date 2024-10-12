0fce538f11644b4c3079152cd878c734a4475817
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:21:42 -0300

    Update categories.module.ts

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,3 +1,14 @@
+/**
+ * Módulo responsável pela gestão das categorias.
+ * 
+ * O `CategoriesModule` agrega o `CategoriesController` e o 
+ * `CategoriesService`, bem como o `PrismaService` para acesso ao 
+ * banco de dados. Este módulo organiza a funcionalidade relacionada 
+ * às categorias, permitindo a injeção de dependências e 
+ * encapsulando a lógica de negócios e os manipuladores de 
+ * rotas associados.
+ */
+
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
