478c02d349acaa37a2c6ef28c23b147c1cd9501d
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:09:21 -0300

    Update prisma.service.ts

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,3 +1,12 @@
+/**
+ * Serviço responsável pela interação com o banco de dados utilizando o Prisma.
+ * 
+ * O `PrismaService` estende a classe `PrismaClient`, proporcionando 
+ * uma interface para realizar operações de leitura e escrita no banco de dados. 
+ * Ele é marcado como `@Injectable()`, permitindo que seja injetado em outros 
+ * serviços ou controladores dentro do aplicativo NestJS.
+ */
+
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
