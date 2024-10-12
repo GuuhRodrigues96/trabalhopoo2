1ff1119bcabd6c2a0e1f5fff8ec8ea1cb7578f23
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:20:41 -0300

    Update categories.controller.spec.ts

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,3 +1,12 @@
+/**
+ * Testes para o `CategoriesController`.
+ * 
+ * Este conjunto de testes verifica a definição e a configuração do 
+ * `CategoriesController`. Utiliza o framework de testes do NestJS para 
+ * criar um módulo de teste e garantir que o controlador está 
+ * devidamente instanciado.
+ */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
 
