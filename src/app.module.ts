9ebc7ad02253d089a2e6c91d092198d414576a31
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:27:56 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,11 @@
+/**
+ * Módulo principal da aplicação (AppModule).
+ * 
+ * Este módulo é responsável por agregar todos os módulos da aplicação,
+ * incluindo o módulo de categorias, usuários e produtos.
+ * Ele também define os controladores e serviços utilizados em toda a aplicação.
+ */
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
