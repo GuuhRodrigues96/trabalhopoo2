bd288d656ffa779f3eb1f842c77bc50bbc201b64
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:28:37 -0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,10 @@
+/**
+ * Serviço principal da aplicação (AppService).
+ * 
+ * Este serviço contém a lógica de negócios principal da aplicação.
+ * Atualmente, possui um método simples que retorna uma mensagem de saudação.
+ */
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
