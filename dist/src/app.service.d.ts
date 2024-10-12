743bfd0ec71aabd72cb96e21b54b228f54362857
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:08:42 -0300

    Update app.service.d.ts

--- dist/src/app.service.d.ts
+++ dist/src/app.service.d.ts
@@ -1,3 +1,13 @@
+/**
+ * Serviço principal da aplicação responsável pela lógica de negócios.
+ * 
+ * O `AppService` contém métodos que implementam a lógica do aplicativo. 
+ * Neste caso, o serviço inclui um método que retorna uma mensagem de saudação.
+ * 
+ * Métodos:
+ * - `getHello()`: Retorna uma mensagem de saudação.
+ */
+
 export declare class AppService {
     getHello(): string;
 }
