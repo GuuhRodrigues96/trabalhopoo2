33651a3bcb1845809f532a37267bb69449869d7f
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:04:09 -0300

    Update products.service.d.ts

--- dist/src/products/products.service.d.ts
+++ dist/src/products/products.service.d.ts
@@ -1,3 +1,17 @@
+/**
+ * Serviço responsável pelas operações relacionadas aos produtos no sistema.
+ * 
+ * O `ProductsService` utiliza o `PrismaService` para realizar operações de 
+ * criação, consulta, atualização e remoção de produtos no banco de dados.
+ * 
+ * Métodos:
+ * - `create(data: { ean: string; name: string; idCategory: number })`: Cria um novo produto.
+ * - `findAll()`: Retorna todos os produtos cadastrados.
+ * - `findOne(id: number)`: Retorna um produto específico com base no ID.
+ * - `update(id: number, data: { ean?: string; name?: string; idCategory?: number })`: Atualiza os dados de um produto existente.
+ * - `remove(id: number)`: Remove um produto com base no ID.
+ */
+
 import { PrismaService } from '../../prisma/prisma.service';
 import { Product } from '@prisma/client';
 export declare class ProductsService {
