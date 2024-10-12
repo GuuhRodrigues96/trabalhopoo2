59a89135cd0d37e7e07a4456aeed7817d2f4b191
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:07:09 -0300

    Update app.controller.d.ts

--- dist/src/app.controller.d.ts
+++ dist/src/app.controller.d.ts
@@ -1,3 +1,15 @@
+/**
+ * Controlador principal do aplicativo responsável por gerenciar as requisições
+ * relacionadas à aplicação.
+ * 
+ * O `AppController` lida com as requisições HTTP e delega as operações
+ * ao `AppService`, que contém a lógica de negócios. Este controlador pode
+ * incluir métodos que respondem a rotas específicas do aplicativo.
+ * 
+ * Métodos:
+ * - `getHello()`: Retorna uma mensagem de saudação.
+ */
+
 import { AppService } from './app.service';
 export declare class AppController {
     private readonly appService;
