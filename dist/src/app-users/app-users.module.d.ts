4bd0288ea249cb2a2f112b932649713ef475401a
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 21:54:13 -0300

    Update app-users.module.d.ts

--- dist/src/app-users/app-users.module.d.ts
+++ dist/src/app-users/app-users.module.d.ts
@@ -1,2 +1,10 @@
+/**
+ * Módulo responsável por gerenciar a funcionalidade de usuários no sistema.
+ * 
+ * O `AppUsersModule` encapsula todas as operações e serviços relacionados
+ * aos usuários do aplicativo. Ele organiza e disponibiliza o controlador e 
+ * o serviço de usuários para serem utilizados em outras partes da aplicação.
+ */
+
 export declare class AppUsersModule {
 }
