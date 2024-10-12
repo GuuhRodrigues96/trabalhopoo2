869686a8293d51697be8e90a619d29393d6aa441
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:15:28 -0300

    Update app-users.controller.spec.ts

--- src/app-users/app-users.controller.spec.ts
+++ src/app-users/app-users.controller.spec.ts
@@ -1,3 +1,12 @@
+/**
+ * Testes para o `AppUsersController`.
+ * 
+ * Este conjunto de testes verifica a definição e a configuração do 
+ * `AppUsersController`. Utiliza o framework de testes do NestJS para 
+ * criar um módulo de teste e garantir que o controlador está 
+ * devidamente instanciado.
+ */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppUsersController } from './app-users.controller';
 
