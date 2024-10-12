e884bc5ff155739ea851cff61d3cbd76b05c18e0
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 21:52:39 -0300

    Update create-app-user.dto.d.ts

--- dist/src/app-users/dto/create-app-user.dto.d.ts
+++ dist/src/app-users/dto/create-app-user.dto.d.ts
@@ -1,3 +1,16 @@
+/**
+ * Data Transfer Object (DTO) para a criação de um usuário.
+ * 
+ * Esta classe contém os campos necessários para a criação de um usuário
+ * no sistema, incluindo nome de usuário, e-mail, senha e tipo de usuário.
+ * 
+ * Campos:
+ * - `username`: Nome de usuário único.
+ * - `email`: E-mail válido associado ao usuário.
+ * - `password`: Senha do usuário, que deve ser armazenada de forma segura.
+ * - `type`: Define o tipo de usuário (por exemplo: 'admin', 'user').
+ */
+
 export declare class CreateAppUserDto {
     username: string;
     email: string;
