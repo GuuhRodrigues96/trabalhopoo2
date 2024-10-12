9e25d26e882f5aa3b1da6dd8639f063a8f300263
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:25:35 -0300

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,10 @@
+/**
+ * Testes para o controlador principal da aplicação (AppController).
+ *
+ * Este arquivo contém testes unitários que verificam a funcionalidade
+ * do controlador, garantindo que o método getHello retorne a resposta esperada.
+ */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
