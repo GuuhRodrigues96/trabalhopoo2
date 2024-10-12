ae01d22b19cfd42bed178a3200b7bc7601ad2cf4
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 22:24:19 -0300

    Update products.module.ts

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,3 +1,11 @@
+/**
+ * Módulo responsável pela gestão dos produtos.
+ * 
+ * Este módulo encapsula o controlador e o serviço relacionados
+ * aos produtos, além de fornecer a instância do `PrismaService`
+ * para a manipulação do banco de dados.
+ */
+
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
