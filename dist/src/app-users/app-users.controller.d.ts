e74b0682df28b097cd95c9de1924e2aab0061e32
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 21:53:42 -0300

    Update app-users.controller.d.ts

--- dist/src/app-users/app-users.controller.d.ts
+++ dist/src/app-users/app-users.controller.d.ts
@@ -1,3 +1,18 @@
+/**
+ * Controlador para gerenciar os usuários do aplicativo.
+ * 
+ * O `AppUsersController` é responsável por lidar com as requisições HTTP
+ * relacionadas aos usuários, como criação, consulta, atualização e remoção
+ * de usuários. Ele delega as operações para o `AppUsersService`.
+ * 
+ * Métodos:
+ * - `create(createAppUserDto: CreateAppUserDto)`: Cria um novo usuário.
+ * - `findAll()`: Retorna todos os usuários cadastrados.
+ * - `findOne(id: string)`: Retorna um usuário específico com base no ID.
+ * - `update(id: string, updateAppUserDto)`: Atualiza os dados de um usuário existente.
+ * - `remove(id: string)`: Remove um usuário com base no ID fornecido.
+ */
+
 import { AppUsersService } from './app-users.service';
 import { AppUser } from '@prisma/client';
 import { CreateAppUserDto } from './dto/create-app-user.dto';
