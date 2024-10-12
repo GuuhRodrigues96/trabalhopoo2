51ce972451d1c21e28edb35674a0fb386c271466
Gustavo Rodrigues <grdo1@aluno.ifnmg.edu.br>
2024-10-11 21:55:51 -0300

    Update categories.controller.d.ts

--- dist/src/categories/categories.controller.d.ts
+++ dist/src/categories/categories.controller.d.ts
@@ -1,3 +1,18 @@
+/**
+ * Controlador responsável pelo gerenciamento das categorias no sistema.
+ * 
+ * O `CategoriesController` lida com as requisições HTTP relacionadas às categorias, 
+ * incluindo a criação, consulta, atualização e remoção de categorias. Ele delega 
+ * essas operações para o `CategoriesService`.
+ * 
+ * Métodos:
+ * - `create(createCategoryDto: { name: string })`: Cria uma nova categoria.
+ * - `findAll()`: Retorna todas as categorias cadastradas.
+ * - `findOne(id: string)`: Retorna uma categoria específica com base no ID.
+ * - `update(id: string, updateCategoryDto: { name: string })`: Atualiza os dados de uma categoria.
+ * - `remove(id: string)`: Remove uma categoria com base no ID.
+ */
+
 import { CategoriesService } from './categories.service';
 import { Category } from '@prisma/client';
 export declare class CategoriesController {
